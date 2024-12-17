97e1b0bc987798198b4f8103c9c31257ac412b04
sharp0904 <0xcrazy0904@gmail.com>
2024-12-17 21:25:16 +0100

    add database

--- members/views.py
+++ members/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
